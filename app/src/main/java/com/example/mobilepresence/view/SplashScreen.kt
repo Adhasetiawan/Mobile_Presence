c8976d10678a7ec199f237784b5c69e6d158a0bf
adha setiawan <adhasetiawanwiyana@gmail.com>
2021-01-13 14:39:53 +0800

    splah has been update

--- app/src/main/java/com/example/mobilepresence/view/SplashScreen.kt
+++ app/src/main/java/com/example/mobilepresence/view/SplashScreen.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mobilepresence.R
-import com.example.mobilepresence.login.LoginActivity
+import com.example.mobilepresence.view.login.LoginActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class SplashScreen : AppCompatActivity() {
@@ -12,8 +12,8 @@ class SplashScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        logo.alpha = 0f
-        logo.animate().setDuration(1500).alpha(1f).withEndAction {
+        placer.alpha = 0f
+        placer.animate().setDuration(1500).alpha(1f).withEndAction {
             val i = Intent(this, LoginActivity::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
--- app/src/main/java/com/example/mobilepresence/view/login/LoginActivity.kt
+++ app/src/main/java/com/example/mobilepresence/view/login/LoginActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mobilepresence.login
+package com.example.mobilepresence.view.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
