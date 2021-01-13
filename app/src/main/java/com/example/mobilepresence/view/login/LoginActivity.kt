fc19b0a383fea9364f30679446007828071857d8
adha setiawan <adhasetiawanwiyana@gmail.com>
2021-01-13 15:59:00 +0800

    setting layout must be fix fast

--- app/src/main/java/com/example/mobilepresence/view/login/LoginActivity.kt
+++ app/src/main/java/com/example/mobilepresence/view/login/LoginActivity.kt
@@ -2,11 +2,17 @@ package com.example.mobilepresence.view.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.mobilepresence.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btn_login.setOnClickListener{
+            Toast.makeText(this, "layout berhasil", Toast.LENGTH_SHORT).show()
+        }
     }
 }
