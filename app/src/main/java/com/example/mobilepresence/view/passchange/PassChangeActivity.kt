e89a0704154093d0923bababc0a930c5dbc86dfb
adha setiawan <adhasetiawanwiyana@gmail.com>
2021-02-12 22:26:18 +0800

    Pass change page has been added

--- app/src/main/java/com/example/mobilepresence/view/passchange/PassChangeActivity.kt
+++ app/src/main/java/com/example/mobilepresence/view/passchange/PassChangeActivity.kt
@@ -1,8 +1,11 @@
 package com.example.mobilepresence.view.passchange
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mobilepresence.databinding.ActivityPassChangeBinding
+import com.example.mobilepresence.view.bottomnav.BottomNavActivity
+import org.jetbrains.anko.design.snackbar
 
 class PassChangeActivity : AppCompatActivity() {
 
@@ -12,5 +15,11 @@ class PassChangeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityPassChangeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnConfirmChange.setOnClickListener {
+            snackbar(binding.btnConfirmChange, "This page is working fine!")
+            startActivity(Intent(this, BottomNavActivity::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/mobilepresence/view/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/mobilepresence/view/profile/ProfileFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.example.mobilepresence.BuildConfig
 import com.example.mobilepresence.databinding.FragmentProfileBinding
 import com.example.mobilepresence.util.loadImageFromUrl
@@ -42,7 +41,7 @@ class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View{
         // Inflate the layout for this fragment
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
